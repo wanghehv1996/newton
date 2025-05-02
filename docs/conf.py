9260b6842f8d05ff99af6731e82d52242607d516
Eric Shi <ershi@nvidia.com>
2025-05-02 16:21:15 -0700

    Comment out html_static_path
    Signed-off-by: Eric Shi <ershi@nvidia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -85,4 +85,4 @@ autodoc_mock_imports = ["jax", "torch", "paddle", "pxr"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
