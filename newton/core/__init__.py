7047e2650bec68ced72f7ee7fdf30db736374b64
Viktor Reutskyy <vreutskyy@nvidia.com>
2025-05-08 12:00:31 +0200

    unused import
    Signed-off-by: Viktor Reutskyy <vreutskyy@nvidia.com>

--- newton/core/__init__.py
+++ newton/core/__init__.py
@@ -15,7 +15,15 @@
 
 from .contact import Contact
 from .control import Control
-from .model import SHAPE_FLAG_COLLIDE_SHAPES, SHAPE_FLAG_VISIBLE, Model
+from .model import SHAPE_FLAG_COLLIDE_GROUND, SHAPE_FLAG_COLLIDE_SHAPES, SHAPE_FLAG_VISIBLE, Model
 from .state import State
 
-__all__ = ["Contact", "Control", "Model", "State"]
+__all__ = [
+    "SHAPE_FLAG_COLLIDE_GROUND",
+    "SHAPE_FLAG_COLLIDE_SHAPES",
+    "SHAPE_FLAG_VISIBLE",
+    "Contact",
+    "Control",
+    "Model",
+    "State",
+]
