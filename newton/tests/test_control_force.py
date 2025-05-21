fca2880340f94eba8743ad3a7c1a22364ce9b6df
Eric Heiden <eric-heiden@outlook.com>
2025-05-20 18:13:24 -0700

    Re-enable all devices for control force test
    Signed-off-by: Eric Heiden <eric-heiden@outlook.com>

--- newton/tests/test_control_force.py
+++ newton/tests/test_control_force.py
@@ -23,7 +23,7 @@ import numpy as np
 import warp as wp
 
 import newton
-from newton.tests.unittest_utils import add_function_test
+from newton.tests.unittest_utils import add_function_test, get_test_devices
 
 wp.config.quiet = True
 
@@ -126,7 +126,7 @@ def test_3d_articulation(test: TestControlForce, device, solver_fn):
             test.assertAlmostEqual(qd[i], 0.0, delta=1e-6)
 
 
-devices = ["cpu"]  # get_test_devices()
+devices = get_test_devices()
 solvers = {
     # "featherstone": lambda model: newton.solvers.FeatherstoneSolver(model, angular_damping=0.0),
     "mujoco_c": lambda model: newton.solvers.MuJoCoSolver(
@@ -158,5 +158,5 @@ for device in devices:
         )
 
 if __name__ == "__main__":
-    # wp.clear_kernel_cache()
+    wp.clear_kernel_cache()
     unittest.main(verbosity=2)
