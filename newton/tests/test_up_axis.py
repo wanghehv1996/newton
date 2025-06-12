1261d8d4bc27e926c838f21850826b6485a2f688
Eric Heiden <eheiden@nvidia.com>
2025-06-12 09:51:38 +0200

    Fix tests
    Signed-off-by: Eric Heiden <eheiden@nvidia.com>

--- newton/tests/test_up_axis.py
+++ newton/tests/test_up_axis.py
@@ -52,10 +52,10 @@ devices = get_test_devices()
 solvers = {
     "featherstone": lambda model: newton.solvers.FeatherstoneSolver(model, angular_damping=0.0),
     "mujoco_c": lambda model: newton.solvers.MuJoCoSolver(
-        model, use_mujoco=True, update_data_every=0, disable_contacts=True
+        model, use_mujoco=True, update_data_interval=0, disable_contacts=True
     ),
     "mujoco_warp": lambda model: newton.solvers.MuJoCoSolver(
-        model, use_mujoco=False, update_data_every=0, disable_contacts=True
+        model, use_mujoco=False, update_data_interval=0, disable_contacts=True
     ),
     "xpbd": lambda model: newton.solvers.XPBDSolver(model, angular_damping=0.0),
     "semi_implicit": lambda model: newton.solvers.SemiImplicitSolver(model, angular_damping=0.0),
