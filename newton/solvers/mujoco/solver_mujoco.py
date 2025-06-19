83b29f4824239193471d3ed432d757a6f443b82c
Lukasz Wawrzyniak <lwawrzyniak@nvidia.com>
2025-06-19 17:08:31 +0200

    Fix Mujoco axis regression (#253)

--- newton/solvers/mujoco/solver_mujoco.py
+++ newton/solvers/mujoco/solver_mujoco.py
@@ -1256,6 +1256,9 @@ class MuJoCoSolver(SolverBase):
                 if stype in (newton.GEO_CAPSULE, newton.GEO_CYLINDER):
                     # mujoco aligns these shapes with the z-axis, Warp uses the y-axis
                     q = q * rot_y2z
+                if model.up_axis == 2 and warp_body_id == -1:
+                    # reverse rotation that aligned the z-axis with the y-axis
+                    q = q * wp.quat_inverse(rot_y2z)
                 if perm_position:
                     # mujoco aligns these shapes with the z-axis, Warp uses the y-axis
                     p = wp.vec3(p[0], -p[2], p[1])
