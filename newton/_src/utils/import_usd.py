Fix quaternion inversion in USD importer (#559)
Signed-off-by: Alain Denzler <adenzler@nvidia.com>

newton/_src/utils/import_usd.py
@@ -177,10 +177,7 @@ def parse_usd(
         if not attr or not attr.HasAuthoredValue():
             return default
         val = attr.Get()
-        if invert_rotations:
-            quat = wp.quat(*val.imaginary, -val.real)
-        else:
-            quat = wp.quat(*val.imaginary, val.real)
+        quat = from_gfquat(val)
         l = wp.length(quat)
         if np.isfinite(l) and l > 0.0:
             return quat