9d51f808985798dbefc81856880d5b12c4dd6cdc
camevor <camevor@nvidia.com>
2025-08-21 12:22:03 +0200

    Rename custom USD attributes' prefix from `warp` to `newton` (#589)
    Signed-off-by: camevor <camevor@nvidia.com>

--- newton/_src/utils/import_usd.py
+++ newton/_src/utils/import_usd.py
@@ -64,12 +64,12 @@ def parse_usd(
         default_density (float): The default density to use for bodies without a density attribute.
         only_load_enabled_rigid_bodies (bool): If True, only rigid bodies which do not have `physics:rigidBodyEnabled` set to False are loaded.
         only_load_enabled_joints (bool): If True, only joints which do not have `physics:jointEnabled` set to False are loaded.
-        joint_drive_gains_scaling (float): The default scaling of the PD control gains (stiffness and damping), if not set in the PhysicsScene with as "warp:joint_drive_gains_scaling".
+        joint_drive_gains_scaling (float): The default scaling of the PD control gains (stiffness and damping), if not set in the PhysicsScene with as "newton:joint_drive_gains_scaling".
         invert_rotations (bool): If True, inverts any rotations defined in the shape transforms.
         verbose (bool): If True, print additional information about the parsed USD file.
         ignore_paths (List[str]): A list of regular expressions matching prim paths to ignore.
         cloned_env (str): The prim path of an environment which is cloned within this USD file. Siblings of this environment prim will not be parsed but instead be replicated via `ModelBuilder.add_builder(builder, xform)` to speed up the loading of many instantiated environments.
-        collapse_fixed_joints (bool): If True, fixed joints are removed and the respective bodies are merged. Only considered if not set on the PhysicsScene with as "warp:collapse_fixed_joints".
+        collapse_fixed_joints (bool): If True, fixed joints are removed and the respective bodies are merged. Only considered if not set on the PhysicsScene with as "newton:collapse_fixed_joints".
         enable_self_collisions (bool): Determines the default behavior of whether self-collisions are enabled for all shapes. If a shape has the attribute ``physxArticulation:enabledSelfCollisions`` defined, this attribute takes precedence.
         apply_up_axis_from_stage (bool): If True, the up axis of the stage will be used to set :attr:`newton.ModelBuilder.up_axis`. Otherwise, the stage will be rotated such that its up axis aligns with the builder's up axis. Default is False.
         root_path (str): The USD path to import, defaults to "/".
@@ -488,7 +488,7 @@ def parse_usd(
         path = str(prim.GetPath())
 
         body_armature = parse_float_with_fallback(
-            (prim, physics_scene_prim), "warp:armature", builder.default_body_armature
+            (prim, physics_scene_prim), "newton:armature", builder.default_body_armature
         )
 
         if add_body_to_builder:
@@ -543,10 +543,10 @@ def parse_usd(
             "enabled": joint_desc.jointEnabled,
         }
         current_joint_limit_ke = parse_float_with_fallback(
-            (joint_prim, physics_scene_prim), "warp:joint_limit_ke", default_joint_limit_ke
+            (joint_prim, physics_scene_prim), "newton:joint_limit_ke", default_joint_limit_ke
         )
         current_joint_limit_kd = parse_float_with_fallback(
-            (joint_prim, physics_scene_prim), "warp:joint_limit_kd", default_joint_limit_kd
+            (joint_prim, physics_scene_prim), "newton:joint_limit_kd", default_joint_limit_kd
         )
 
         if key == UsdPhysics.ObjectType.FixedJoint:
@@ -734,7 +734,7 @@ def parse_usd(
 
         # Updating joint_drive_gains_scaling if set of the PhysicsScene
         joint_drive_gains_scaling = parse_float(
-            physics_scene_prim, "warp:joint_drive_gains_scaling", joint_drive_gains_scaling
+            physics_scene_prim, "newton:joint_drive_gains_scaling", joint_drive_gains_scaling
         )
     else:
         # builder.up_vector, builder.up_axis = get_up_vector_and_axis(stage)
@@ -850,9 +850,9 @@ def parse_usd(
                 else:
                     usd_prim = stage.GetPrimAtPath(Sdf.Path(key))
                     if "TensorPhysicsArticulationRootAPI" in usd_prim.GetPrimTypeInfo().GetAppliedAPISchemas():
-                        usd_prim.CreateAttribute("physics:warp:articulationIndex", Sdf.ValueTypeNames.UInt, True).Set(
-                            articulation_id
-                        )
+                        usd_prim.CreateAttribute(
+                            "physics:newton:articulation_index", Sdf.ValueTypeNames.UInt, True
+                        ).Set(articulation_id)
                         articulation_roots.append(key)
 
                 if key in body_specs:
@@ -1011,12 +1011,12 @@ def parse_usd(
                     "body": body_id,
                     "xform": shape_xform,
                     "cfg": ModelBuilder.ShapeConfig(
-                        ke=parse_float_with_fallback(prim_and_scene, "warp:contact_ke", builder.default_shape_cfg.ke),
-                        kd=parse_float_with_fallback(prim_and_scene, "warp:contact_kd", builder.default_shape_cfg.kd),
-                        kf=parse_float_with_fallback(prim_and_scene, "warp:contact_kf", builder.default_shape_cfg.kf),
-                        ka=parse_float_with_fallback(prim_and_scene, "warp:contact_ka", builder.default_shape_cfg.ka),
+                        ke=parse_float_with_fallback(prim_and_scene, "newton:contact_ke", builder.default_shape_cfg.ke),
+                        kd=parse_float_with_fallback(prim_and_scene, "newton:contact_kd", builder.default_shape_cfg.kd),
+                        kf=parse_float_with_fallback(prim_and_scene, "newton:contact_kf", builder.default_shape_cfg.kf),
+                        ka=parse_float_with_fallback(prim_and_scene, "newton:contact_ka", builder.default_shape_cfg.ka),
                         thickness=parse_float_with_fallback(
-                            prim_and_scene, "warp:contact_thickness", builder.default_shape_cfg.thickness
+                            prim_and_scene, "newton:contact_thickness", builder.default_shape_cfg.thickness
                         ),
                         mu=material.dynamicFriction,
                         restitution=material.restitution,
@@ -1200,7 +1200,7 @@ def parse_usd(
     collapse_results = None
     merged_body_data = {}
     path_body_relative_transform = {}
-    if scene_attributes.get("warp:collapse_fixed_joints", collapse_fixed_joints):
+    if scene_attributes.get("newton:collapse_fixed_joints", collapse_fixed_joints):
         collapse_results = builder.collapse_fixed_joints()
         body_merged_parent = collapse_results["body_merged_parent"]
         body_merged_transform = collapse_results["body_merged_transform"]
