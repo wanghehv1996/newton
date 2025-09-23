137ee4b2ffd4f42b522e04f2d7e63c90eef584f7
Philipp Reist <preist@nvidia.com>
2025-09-23 15:58:21 +0200

    Change go2 asset in robot policy to use new USDA file

--- newton/examples/robot/example_robot_policy.py
+++ newton/examples/robot/example_robot_policy.py
@@ -69,7 +69,7 @@ ROBOT_CONFIGS = {
     "go2": RobotConfig(
         asset_dir="unitree_go2",
         policy_path={"mjw": "rl_policies/mjw_go2.pt", "physx": "rl_policies/physx_go2.pt"},
-        asset_path="usd/go2.usd",
+        asset_path="usd/go2.usda",
         yaml_path="rl_policies/go2.yaml",
     ),
     "g1_29dof": RobotConfig(
